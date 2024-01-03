8462c298121d2be8a6e9e3d8affc5bd2561d21a1
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2024-01-03 23:10:18 +0800

    Fix ONNX conversion

--- model/inception_iccv.py
+++ model/inception_iccv.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.tensor as tensor
+from torch import tensor
 from torch.nn import functional as F
 
 __all__ = ['inception_iccv']
@@ -23,17 +23,39 @@ def inception_iccv(pretrained=True, debug=False, **kwargs):
         model.load_state_dict(model_dict)
     return model
 
+# https://github.com/pytorch/pytorch/issues/30563#issuecomment-1295761759
+def affine_grid(theta, size, device='cuda', align_corners=False):
+    N, C, H, W = size
+    grid = create_grid(N, C, H, W)
+    grid = grid.view(N, H * W, 3).bmm(theta.transpose(1, 2))
+    grid = grid.view(N, H, W, 2)
+    return grid.to(device)
+
+def create_grid(N, C, H, W):
+    grid = torch.empty((N, H, W, 3), dtype=torch.float32)
+    grid = torch.empty((N, H, W, 3), dtype=torch.float32)
+    grid.select(-1, 0).copy_(linspace_from_neg_one(W))
+    grid.select(-1, 1).copy_(linspace_from_neg_one(H).unsqueeze_(-1))
+    grid.select(-1, 2).fill_(1)
+    return grid
+
+def linspace_from_neg_one(num_steps, dtype=torch.float32):
+    r = torch.linspace(-1, 1, num_steps, dtype=torch.float32)
+    r = r * (num_steps - 1) / num_steps
+    return r
 
 class ChannelAttn(nn.Module):
-    def __init__(self, in_channels, reduction_rate=16):
+    def __init__(self, in_channels, pooling_size, reduction_rate=16):
         super(ChannelAttn, self).__init__()
         assert in_channels%reduction_rate == 0
         self.conv1 = nn.Conv2d(in_channels, in_channels // reduction_rate, kernel_size=1, stride=1, padding=0)
         self.conv2 = nn.Conv2d(in_channels // reduction_rate, in_channels, kernel_size=1, stride=1, padding=0)
+        self.pooling_size = pooling_size
 
     def forward(self, x):
         # squeeze operation (global average pooling)
-        x = F.avg_pool2d(x, x.size()[2:])
+        x_size = [self.pooling_size, self.pooling_size//2]
+        x = F.avg_pool2d(x, x_size)
         # excitation operation (2 conv layers)
         x = F.relu(self.conv1(x))
         x = self.conv2(x)
@@ -55,13 +77,13 @@ class SpatialTransformBlock(nn.Module):
         for i in range(self.num_classes):
             self.gap_list.append(nn.AvgPool2d((pooling_size, pooling_size//2), stride=1, padding=0, ceil_mode=True, count_include_pad=True))
             self.fc_list.append(nn.Linear(channels, 1))
-            self.att_list.append(ChannelAttn(channels))
+            self.att_list.append(ChannelAttn(channels, pooling_size))
             self.stn_list.append(nn.Linear(channels, 4))
 
     def stn(self, x, theta):
-        grid = F.affine_grid(theta, x.size())
-        x = F.grid_sample(x, grid, padding_mode='border')
-        return x.cuda()
+        grid = affine_grid(theta, x.size(), device=x.device.type, align_corners=True)
+        x = F.grid_sample(x, grid, padding_mode='border', align_corners=True)
+        return x
 
     def transform_theta(self, theta_i, region_idx):
         theta = torch.zeros(theta_i.size(0), 2, 3)
@@ -69,7 +91,6 @@ class SpatialTransformBlock(nn.Module):
         theta[:,1,1] = torch.sigmoid(theta_i[:,1])
         theta[:,0,2] = torch.tanh(theta_i[:,2])
         theta[:,1,2] = torch.tanh(theta_i[:,3])
-        theta = theta.cuda()
         return theta
 
     def forward(self, features):
@@ -77,8 +98,8 @@ class SpatialTransformBlock(nn.Module):
         bs = features.size(0)
         for i in range(self.num_classes):
             stn_feature = features * self.att_list[i](features) + features
-
-            theta_i = self.stn_list[i](F.avg_pool2d(stn_feature, stn_feature.size()[2:]).view(bs,-1)).view(-1,4)
+            stn_feature_size = [int(self.spatial), int(self.spatial//2)]
+            theta_i = self.stn_list[i](F.avg_pool2d(stn_feature, stn_feature_size).view(bs,-1)).view(-1,4)
             theta_i = self.transform_theta(theta_i, i)
 
             sub_feature = self.stn(stn_feature, theta_i)
